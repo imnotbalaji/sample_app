79ff13b3cac2b99090a0212c4364333c6ed9f5d6
Balaji <imnotbalaji@gmail.com>
2025-05-14 10:14:50 -0400

    Complete advanced testing setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 require "minitest/reporters"
+Minitest::Reporters.use! 
+Minitest::Reporters::SpecReporter.new
 
 module ActiveSupport
   class TestCase
@@ -15,4 +17,4 @@ module ActiveSupport
   end
 end
 
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
